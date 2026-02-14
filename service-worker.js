92ad83035ea11f5dc8b1371b180ff41c5a536679
HariPrasath260805 <harimuthu693@gmail.com>
2026-02-14 13:21:09 +0530

    commit

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,7 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker Installed");
+self.addEventListener("install", function(event) {
+  console.log("Chatbot Service Worker Installed");
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(fetch(event.request));
 });
